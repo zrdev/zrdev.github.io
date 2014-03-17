05bec11b9add7182a84769cc001a2a7f7bc0f7ec
Ethan DiNinno <dininno@mit.edu>
2014-03-17 14:27:03 -0400

    Working state 3/17

--- js/controllers/new-project.js
+++
@@ -1,6 +0,0 @@
-zr.controller('NewProjectController', function($scope, $modal) {
-	$modal.open({
-		templateUrl: 'partials/new-project-modal.html',
-		controller: 'NewProjectModalController'
-	});
-});
