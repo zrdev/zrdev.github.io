2e2241e54dd6167ab58b5c7fa7b00bc8b1238eb6
Ethan DiNinno <dininno@Ethans-MacBook-Pro.local>
2014-04-17 22:20:17 -0400

    Added randomize and reset buttons to sim dialog

--- js/controllers/simulation.js
+++ js/controllers/simulation.js
@@ -1,20 +1,19 @@
 zr.controller('SimulationController', function($scope, $modalInstance, gameResource) {
 	var game = gameResource.data;
-	$scope.data = {
-		sph: 1,
-		opponentId: null,
-		timeout: game.defaultTimeout,
-		sph1init: JSON.parse(game.initState1),
-		sph2init: JSON.parse(game.initState2),
-		gameVars: []
-	};
-	var varsConfig = JSON.parse(game.gameVariables);
-	for(var i = varsConfig.length; i--; ) {
-		$scope.data.gameVars.push({
-			name: varsConfig[i].name,
-			value: varsConfig[i].min
-		});
+	$scope.resetAll = function() {
+		$scope.data = {
+			sph: 1,
+			opponentId: null,
+			timeout: game.defaultTimeout,
+			sph1init: JSON.parse(game.initState1),
+			sph2init: JSON.parse(game.initState2),
+			gameVars: JSON.parse(game.gameVariables)
+		};
+		for(var i = $scope.data.gameVars.length; i--; ) {
+			$scope.data.gameVars[i].value = $scope.data.gameVars[i].min;
+		}
 	}
+	$scope.resetAll();
 
 	$scope.opponentTitle = 'No Opponent';
 	
@@ -27,5 +26,17 @@ zr.controller('SimulationController', function($scope, $modalInstance, gameResou
 	
 	$scope.cancel = function() {
 		$modalInstance.dismiss('cancel');
+	};
+
+	$scope.randomize = function() {
+		for(var i = $scope.data.gameVars.length; i--; ) {
+			var v = $scope.data.gameVars[i];
+			if(v.type === 'int') {
+				v.value = v.min + Math.floor(Math.random() * (v.max - v.min + 1));
+			}
+			else if(v.type === 'float') {
+				v.value = v.min + Math.random() * (v.max - v.min);
+			}
+		}
 	};
 });
