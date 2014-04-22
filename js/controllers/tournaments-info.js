2f3c2c56b3d32bd140a6285bcfb01e6f98319af5
Ethan DiNinno <dininno@Ethans-MacBook-Pro.local>
2014-04-22 14:01:15 -0400

    Fleshing out tournament info pages

--- js/controllers/tournaments-info.js
+++ js/controllers/tournaments-info.js
@@ -1,6 +1,6 @@
 zr.controller('TournamentsInfoController', ['$scope', '$route', 'tournamentResource', function($scope, $route, tournamentResource) {
 	$scope.tournament = tournamentResource.data;
-	
+	$scope.resource = null;
 	//Resolve URL params to state
 	if($route.current.params.mode !== void 0) {
 		$scope.mode = $route.current.params.mode;
@@ -10,20 +10,34 @@ zr.controller('TournamentsInfoController', ['$scope', '$route', 'tournamentResou
 	}
 	if($route.current.params.resourceId !== void 0) {
 		$scope.resourceId = parseInt($route.current.params.resourceId);
-	}
-
-	$scope.setDisplay = function(mode, resourceId) {
-		$scope.mode = mode;
-		$scope.resourceId = resourceId;
-	}
-	
-	$scope.getInfoPage = function() {
-		var len = $scope.tournament.tournamentsInfopageCollection.length;
-		for(var i = 0; i < len; i++) {
-			if($scope.tournament.tournamentsInfopageCollection[i].id === $scope.resourceId) {
-				return $scope.tournament.tournamentsInfopageCollection[i].content;
+		var list;
+		if($scope.mode === 'info') {
+			list = $scope.tournament.tournamentsInfopageCollection;
+		}
+		else if($scope.mode === 'teams') {
+			list = $scope.tournament.tournamentsDivisionCollection;
+		}
+		else if($scope.mode === 'scores') {
+			list = $scope.tournament.tournamentsCompetitionCollection;
+		}
+		else {
+			list = [];
+		}
+		for(var i = list.length; i--; ) {
+			if(list[i].id === $scope.resourceId) {
+				$scope.resource = list[i];
+				break;
 			}
 		}
-		return '';
+	}
+	else { //Root page: display overview
+		for(var i = $scope.tournament.tournamentsInfopageCollection.length; i--; ) {
+			var p = $scope.tournament.tournamentsInfopageCollection[i];
+			if(p.displayOrder === 1) {
+				$scope.resource = p;
+				$scope.resourceId = p.id;
+				break;
+			}
+		}
 	}
 }]);
