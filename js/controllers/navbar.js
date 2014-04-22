507d6291619cc940633a2485c55b4927856aebee
Ethan DiNinno <dininno@Ethans-MacBook-Pro.local>
2014-04-21 23:04:18 -0400

    Logging out redirects to front page

--- js/controllers/navbar.js
+++ js/controllers/navbar.js
@@ -49,6 +49,6 @@ zr.controller('NavbarController', ['$scope', 'realtime', 'drive', 'zrdb',
 		}
 
 		$scope.signOut = function() {
-			window.location.href = 'https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=' + window.location.href;
+			window.location.href = 'https://www.google.com/accounts/Logout?continue=https://appengine.google.com/_ah/logout?continue=' + window.location.origin;
 		}
 }]);
