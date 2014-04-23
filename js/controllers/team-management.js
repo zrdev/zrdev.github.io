0ed0d4938b2197a44607a95a03250c5600c7cc4f
Ethan DiNinno <dininno@Ethans-MacBook-Pro.local>
2014-04-22 20:36:06 -0400

    Team management

--- js/controllers/team-management.js
+++ js/controllers/team-management.js
@@ -1,3 +1,12 @@
 zr.controller('TeamManagementController', ['$scope', 'teamResources', function($scope, teamResources) {
 	$scope.teams = teamResources.groups;
+	$scope.currentTeam = null;
+
+	for(var i = $scope.teams.length; i--; ) {
+		$scope.teams[i].description = JSON.parse($scope.teams[i].description);
+	}
+
+	$scope.setTeam = function(team) {
+		$scope.currentTeam = team; 
+	}
 }]);
