6f1f32715a052f46f71d2f1b60170ffe928300fd
Ethan DiNinno <dininno@Ethans-MacBook-Pro.local>
2014-04-22 21:47:22 -0400

    Team management

--- js/controllers/team-management.js
+++ js/controllers/team-management.js
@@ -1,6 +1,8 @@
 zr.controller('TeamManagementController', ['$scope', 'teamResources', 'realtime', function($scope, teamResources, realtime) {
 	$scope.teams = teamResources.groups;
 	$scope.currentTeam = null;
+	$scope.countries = ['United States', 'Austria', 'Belgium', 'Czech Republic', 'Denmark', 'Finland', 'France', 'Germany', 'Greece', 'Ireland', 'Italy', 'Luxembourg', 'Netherlands', 'Norway', 'Poland', 'Portugal', 'Romania', 'Spain', 'Sweden', 'Switzerland', 'United Kingdom'];
+	$scope.states = ["AL", "AK", "AS", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FM", "FL", "GA", "GU", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MH", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "MP", "OH", "OK", "OR", "PW", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VI", "VA", "WA", "WV", "WI", "WY"];
 
 	for(var i = $scope.teams.length; i--; ) {
 		$scope.teams[i].description = JSON.parse($scope.teams[i].description);
