3fa19860cf2c6d8944e02451c789de91d4fbd48f
Ethan DiNinno <dininno@Ethans-MacBook-Pro.local>
2014-04-28 17:28:52 -0400

    Modified frontpage announcement

--- js/controllers/announcements.js
+++ js/controllers/announcements.js
@@ -6,10 +6,10 @@ zr.controller('AnnouncementsController', ['$scope', '$routeParams', function($sc
 			title: 'Welcome to our new website!',
 			date: 1397848177072,
 			user: 'Ethan DiNinno',
-			content: '<p>We\'ve revamped our website for the 2014 season. Take a look around! </p><p>If you need to retrieve data from the old website, it\'s still available <a href="http://zerorobotics.org/">here</a>.</p>'
-			+ '<p>Some pages on this new website are still under construction. They are clearly labeled as such and we are working on finishing them.</p>'
-			+ '<p>The website now uses Google federated login. Any account used on Gmail, Google Docs, YouTube, etc. will work. If you don\'t have a Google account, you can create one <a href="https://accounts.google.com/SignUp">here</a>.'
-			+ '<p>If you have any questions about this transition, please contact us at <a href="mailto:zerorobotics@mit.edu">zerorobotics@mit.edu</a>.' 
+			content: '<p>We\'ve revamped our website for the 2014 season. Take a look around! </p><p>If you need to retrieve data from the old website, it\'s still available <a href="http://zerorobotics.org/">here</a>. </p>'
+			+ '<p>Some pages on this new website are still under construction. They are clearly labeled as such and we are working on finishing them. </p>'
+			+ '<p>The website now uses Google federated login. Any account used on Gmail, Google Docs, YouTube, etc. will work. If you don\'t have a Google account, you can create one <a href="https://accounts.google.com/SignUp">here</a>. </p>'
+			+ '<p>If you have any questions about this transition, please contact us at <a href="mailto:zerorobotics@mit.edu">zerorobotics@mit.edu</a>. </p>' 
 		}
 	];
 	$scope.data.announcement = null;
