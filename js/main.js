1e3493c9bc298d31c285da6d28c5ca7e600302ca
Ethan DiNinno <dininno@mit.edu>
2014-05-05 18:02:55 -0400

    Firefox dropdown right-click fix

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
+//Patch to prevent dropdowns closing on right-click in Firefox
+angular.element(document).on('click', function(e) {
+  e.button === 2 && e.stopImmediatePropagation();
+})
 //Initial module setup and URL routing
 
 var zr = angular.module('zr', ['ui.bootstrap', 'ui.ace', 'ui.keypress', 'ngRoute', 'ngSanitize', 'uiSlider'])
