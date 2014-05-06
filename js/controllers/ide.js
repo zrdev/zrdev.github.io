4f60ba9c5d2d153e6de536c37bf6a5750128aaf9
Ethan DiNinno <dininno@mit.edu>
2014-05-06 00:39:08 -0400

    Hiding footer in IDE and vis

--- js/controllers/ide.js
+++ js/controllers/ide.js
@@ -1,5 +1,7 @@
 zr.controller('IdeController', ['$scope', '$modal', '$http', '$timeout', '$location', 'config', 'realtime', 'drive', 'resources', 'zrdb', 
 		function($scope, $modal, $http, $timeout, $location, config, realtime, drive, resources, zrdb) {
+	//Hide the footer
+	angular.element('#footer').style['display'] = 'none';
 	var realtimeDocument = resources[0];
 	$scope.model = realtimeDocument.getModel();
 	var graphical = $scope.model.getRoot().get('graphical');
