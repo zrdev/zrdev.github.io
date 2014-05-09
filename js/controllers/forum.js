c7cf5e6742a4fd0657792c984c97b2be34cde1d2
Ethan DiNinno <dininno@mit.edu>
2014-05-09 05:24:12 -0400

    Minor cleanup

--- js/controllers/forum.js
+++ js/controllers/forum.js
@@ -1,5 +1,5 @@
 zr.controller('ForumController', ['$scope', '$sce', function($scope, $sce) {
 	$scope.forumUrl = $sce.trustAsResourceUrl('https://groups.google.com/a/zerorobotics.mit.edu/forum/embed/?place=forum/forum'
-	    + '&showsearch=true&showpopout=false&showtabs=false&hideforumtitle=true'
+	    + '&showsearch=true&showpopout=false&showtabs=false'
 	    + '&parenturl=' + encodeURIComponent(window.location.href));
 }]);
