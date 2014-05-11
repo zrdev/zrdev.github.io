64b84c920030d1bb5fb137cfd6f70ef14b8abf7d
Ethan DiNinno <dininno@mit.edu>
2014-05-11 05:15:08 -0400

    Added view-by-view auth check

--- js/controllers/navbar.js
+++ js/controllers/navbar.js
@@ -48,49 +48,13 @@ zr.controller('NavbarController', ['$scope', 'drive', 'zrdb', '$location', '$tim
 			});
 		};
 
-		window.loginCallback = function(authResult) {
-			if(authResult['status']['signed_in']) {
-				getUser();
-				if(window.authDeferred) {
-					window.authDeferred.resolve();
-					window.authDeferred = null;
-				}
-			}
-			else if(authResult['error']) {
-				$scope.displayName = null;
-				if(window.authDeferred) {
-					window.authDeferred.reject();
-					window.authDeferred = null;
-				}
-			}
-			$scope.showLogin = true;
-		};
+
 
 		$scope.signOut = function() {
 			gapi.auth.signOut();
 		}
 
-		//Render the login button
-		gapi.signin.render('login-button', {
-			'width': 'wide'
-		});
-
-		//Load the Drive APIs - this should be lazy loaded in the future
-		gapi.load('client:drive-share:drive-realtime', function () {
-			// Monkey patch collaborative string for ng-model compatibility
-			Object.defineProperty(gapi.drive.realtime.CollaborativeString.prototype, 'text', {
-				set: gapi.drive.realtime.CollaborativeString.prototype.setText,
-				get: gapi.drive.realtime.CollaborativeString.prototype.getText
-			});
-			
-			//This code copied from blockly/core/realtime.js; register Blockly custom types
-			var custom = gapi.drive.realtime.custom;
-			custom.registerType(Blockly.Block, 'Block');
-			Blockly.Block.prototype.id = custom.collaborativeField('id');
-			Blockly.Block.prototype.xmlDom = custom.collaborativeField('xmlDom');
-			Blockly.Block.prototype.relativeX = custom.collaborativeField('relativeX');
-			Blockly.Block.prototype.relativeY = custom.collaborativeField('relativeY');
-			custom.setInitializer(Blockly.Block, Blockly.Block.prototype.initialize);
-			gapi.client.setApiKey(config.simpleApiKey);
-		});
+		gapi.client.setApiKey(config.simpleApiKey);
+		//Give global reference for login callback
+		zr.navbarScope = $scope;
 }]);
--- js/main.js
+++ js/main.js
@@ -4,13 +4,6 @@ document.onclick = function(e) {
 	e.button === 2 && e.stopImmediatePropagation();
 };
 
-//Callback to bootstrap Angular AFTER the Google APIs are loaded
-document.onGapiLoaded = function() {
-	angular.element(document).ready(function () {
-		angular.bootstrap(document, ['zr']);
-	});
-}
-
 //Initial module setup and URL routing
 
 var zr = angular.module('zr', ['ui.bootstrap', 'ui.ace', 'ui.keypress', 'ngRoute', 'ngSanitize', 'uiSlider'])
@@ -229,3 +222,51 @@ var zr = angular.module('zr', ['ui.bootstrap', 'ui.ace', 'ui.keypress', 'ngRoute
 	//NO TRAILING SLASH on serviceDomain
 	serviceDomain: 'http://zrwebsite-env.elasticbeanstalk.com'
 });
+
+//Callback to bootstrap Angular AFTER the Google APIs are loaded
+window.onGapiLoaded = function() {
+	// Monkey patch collaborative string for ng-model compatibility
+	Object.defineProperty(gapi.drive.realtime.CollaborativeString.prototype, 'text', {
+		set: gapi.drive.realtime.CollaborativeString.prototype.setText,
+		get: gapi.drive.realtime.CollaborativeString.prototype.getText
+	});
+	
+	//This code copied from blockly/core/realtime.js; register Blockly custom types
+	var custom = gapi.drive.realtime.custom;
+	custom.registerType(Blockly.Block, 'Block');
+	Blockly.Block.prototype.id = custom.collaborativeField('id');
+	Blockly.Block.prototype.xmlDom = custom.collaborativeField('xmlDom');
+	Blockly.Block.prototype.relativeX = custom.collaborativeField('relativeX');
+	Blockly.Block.prototype.relativeY = custom.collaborativeField('relativeY');
+	custom.setInitializer(Blockly.Block, Blockly.Block.prototype.initialize);
+
+	//Render the login button
+	gapi.signin.render('login-button', {
+		'width': 'wide'
+	});
+
+	//Bootstrap Angular
+	angular.element(document).ready(function () {
+		angular.bootstrap(document, ['zr']);
+	});
+}
+
+window.loginCallback = function(authResult) {
+	if(authResult['status']['signed_in']) {
+		if(zr.navbarScope) {
+			navbarScope.getUser();
+		}
+		if(window.authDeferred) {
+			window.authDeferred.resolve();
+			window.authDeferred = null;
+		}
+	}
+	else if(authResult['error']) {
+		$scope.displayName = null;
+		if(window.authDeferred) {
+			window.authDeferred.reject();
+			window.authDeferred = null;
+		}
+	}
+};
+zr.navbarScope = null;
