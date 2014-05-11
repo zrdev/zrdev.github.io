71320cabf8afae931070462f50afdeb370d7167f
Ethan DiNinno <dininno@mit.edu>
2014-05-11 07:42:29 -0400

    Don't render auth button until auth has failed once

--- js/controllers/navbar.js
+++ js/controllers/navbar.js
@@ -28,7 +28,7 @@ zr.controller('NavbarController', ['$scope', 'drive', 'zrdb', '$location', '$tim
 			 		}
 			 		else {
 			 			$scope.displayName = null;
-			 			if(firstTime && data) {
+			 			if(firstTime && gapi.auth.getToken()) {
 							//Render the login button
 							gapi.signin.render('login-button', {
 								'width': 'wide'
