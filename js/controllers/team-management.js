8bf1ede8a8701dc1e59232e321ec6565560328d3
Ethan DiNinno <dininno@mit.edu>
2014-05-25 00:22:30 -0400

    Team management

--- js/controllers/team-management.js
+++ js/controllers/team-management.js
@@ -2,7 +2,7 @@ zr.controller('TeamManagementController', ['$scope', 'teamResources', 'zrdb', fu
 	$scope.teams = teamResources.data.rows;
 	$scope.currentTeam = null;
 	$scope.isLead = true;
-	$scope.newEmail = '';
+	$scope.newmem.email = '';
 	$scope.countries = ['United States', 'Austria', 'Belgium', 'Czech Republic', 'Denmark', 'Finland', 'France', 'Germany', 'Greece', 'Ireland', 'Italy', 'Luxembourg', 'Netherlands', 'Norway', 'Poland', 'Portugal', 'Romania', 'Spain', 'Sweden', 'Switzerland', 'United Kingdom'];
 	$scope.states = ["", "AK", "AL", "AR", "AS", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "FM", "GA", "GU", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MH", "MI", "MN", "MO", "MP", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "PR", "PW", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VI", "VT", "WA", "WI", "WV", "WY"];
 
@@ -26,7 +26,7 @@ zr.controller('TeamManagementController', ['$scope', 'teamResources', 'zrdb', fu
 			'inviteAccepted': false,
 			'isLead': false,
 			'teamId': $scope.currentTeam.id,
-			'email': $scope.newEmail,
+			'email': $scope.newmem.email,
 			'tournamentId': 17 //TODO: Separate rosters for different tournaments
 		});
 		$scope.newEmail = '';
