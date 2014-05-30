1e527cc9548730e74abf8476d71a6b293f24d86a
Ethan DiNinno <dininno@mit.edu>
2014-05-31 00:00:52 +0200

    Make QA site use QA backend

--- js/main.js
+++ js/main.js
@@ -191,7 +191,7 @@ var zr = angular.module('zr', ['ui.bootstrap', 'ui.ace', 'ui.keypress', 'ngRoute
 	simpleApiKey: 'AIzaSyCXIUmc4SlS1uW0SL5jDy9JjbIT6jqLIQk',
 	scopes: 'https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.install profile email https://www.googleapis.com/auth/admin.directory.group',
 	//NO TRAILING SLASH on serviceDomain
-	serviceDomain: 'http://zrwebsite-env.elasticbeanstalk.com'
+	serviceDomain: (window.location.hostname === 'zerorobotics.mit.edu' ? 'http://zrwebsite-env.elasticbeanstalk.com' : 'http://zrdev.elasticbeanstalk.com')
 });
 
 //Callback to bootstrap Angular AFTER the Google APIs are loaded
